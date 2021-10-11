144724455811e482323d21f0c3fc2f636c269ae0
Keerthi <80613202+keerthisuresh01@users.noreply.github.com>
2021-10-11 21:17:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var http=require('http');
+var url=require('url');
+var querystring=require('querystring');
+var fs=require('fs');
+const express = require('express');
+const app=express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    fs.readFile('index.html',null,function(err,data){
+        if(err){
+            res.write("error");
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    });
+});
+app.post("/",(res,req)=>{
+    
+})
+app.listen(PORT);
+console.log(PORT);
