41e75d164e0c8cc78cf511c896c2a0f2b2a75fa2
Keerthi <80613202+keerthisuresh01@users.noreply.github.com>
2021-10-11 21:36:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const express = require('express');
 const app=express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    fs.readFile('index.html',null,function(err,data){
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.html',null,function(err,data){
+        console.log("inside err check");
         if(err){
-            res.write("error");
+            res.writeHead(404);
+            res.write("File not found");
         }
         else{
             res.write(data);
-            res.end();
+            res.end();  
         }
-    });
-});
-app.listen(PORT);
+    })
+}).listen(PORT);
 console.log(PORT);
